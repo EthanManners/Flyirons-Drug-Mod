ffc7911e72449c6e074d23f04b7845607e3d5988
Ethan Manners <ethanjmanners@gmail.com>
2026-02-26 22:06:55 -0600

    Fix block/item settings registry keys for 1.21.4 startup

--- src/fabricmain/java/com/flyirons/drugmod/registry/ModBlocks.java
+++ src/fabricmain/java/com/flyirons/drugmod/registry/ModBlocks.java
@@ -12,22 +12,38 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public final class ModBlocks {
     private ModBlocks() {}
 
+    private static RegistryKey<Block> blockKey(String id) {
+        return RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(DrugMod.MOD_ID, id));
+    }
+
     public static final Block EMBERLEAF_CROP = Registry.register(Registries.BLOCK,
             Identifier.of(DrugMod.MOD_ID, "emberleaf_crop"),
-            new EmberleafCropBlock(AbstractBlock.Settings.create().mapColor(MapColor.DARK_GREEN).noCollision().ticksRandomly().breakInstantly().sounds(BlockSoundGroup.CROP)));
+            new EmberleafCropBlock(AbstractBlock.Settings.create()
+                    .registryKey(blockKey("emberleaf_crop"))
+                    .mapColor(MapColor.DARK_GREEN)
+                    .noCollision()
+                    .ticksRandomly()
+                    .breakInstantly()
+                    .sounds(BlockSoundGroup.CROP)));
 
     public static final Block COMPRESSION_BENCH = Registry.register(Registries.BLOCK,
             Identifier.of(DrugMod.MOD_ID, "compression_bench"),
-            new CompressionBenchBlock(AbstractBlock.Settings.create().strength(2.2f).sounds(BlockSoundGroup.METAL)));
+            new CompressionBenchBlock(AbstractBlock.Settings.create()
+                    .registryKey(blockKey("compression_bench"))
+                    .strength(2.2f)
+                    .sounds(BlockSoundGroup.METAL)));
 
     public static void register() {
-        Registry.register(Registries.ITEM, Identifier.of(DrugMod.MOD_ID, "compression_bench"), new BlockItem(COMPRESSION_BENCH, new Item.Settings()));
+        Registry.register(Registries.ITEM, Identifier.of(DrugMod.MOD_ID, "compression_bench"),
+                new BlockItem(COMPRESSION_BENCH, new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(DrugMod.MOD_ID, "compression_bench")))));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> entries.add(COMPRESSION_BENCH));
     }
 }
--- src/fabricmain/java/com/flyirons/drugmod/registry/ModItems.java
+++ src/fabricmain/java/com/flyirons/drugmod/registry/ModItems.java
@@ -9,18 +9,24 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public final class ModItems {
     private ModItems() {}
 
-    public static final Item EMBERLEAF_SEEDS = register("emberleaf_seeds", new BlockItem(ModBlocks.EMBERLEAF_CROP, new Item.Settings()));
-    public static final Item EMBERLEAF_BUNDLE = register("emberleaf_bundle", new Item(new Item.Settings()));
-    public static final Item EMPTY_CART_SHELL = register("empty_cart_shell", new Item(new Item.Settings()));
-    public static final Item TAINTED_POWDER = register("tainted_powder", new Item(new Item.Settings()));
+    private static Item.Settings settings(String id) {
+        return new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(DrugMod.MOD_ID, id)));
+    }
 
-    public static final Item GREEN_HAZE_JOINT = register("green_haze_joint", new DrugConsumableItem(new Item.Settings().maxCount(16), DrugDefinition.GREEN_HAZE));
-    public static final Item SPARK_CART_ITEM = register("spark_cart", new DrugConsumableItem(new Item.Settings().maxCount(16), DrugDefinition.SPARK_CART));
+    public static final Item EMBERLEAF_SEEDS = register("emberleaf_seeds", new BlockItem(ModBlocks.EMBERLEAF_CROP, settings("emberleaf_seeds")));
+    public static final Item EMBERLEAF_BUNDLE = register("emberleaf_bundle", new Item(settings("emberleaf_bundle")));
+    public static final Item EMPTY_CART_SHELL = register("empty_cart_shell", new Item(settings("empty_cart_shell")));
+    public static final Item TAINTED_POWDER = register("tainted_powder", new Item(settings("tainted_powder")));
+
+    public static final Item GREEN_HAZE_JOINT = register("green_haze_joint", new DrugConsumableItem(settings("green_haze_joint").maxCount(16), DrugDefinition.GREEN_HAZE));
+    public static final Item SPARK_CART_ITEM = register("spark_cart", new DrugConsumableItem(settings("spark_cart").maxCount(16), DrugDefinition.SPARK_CART));
 
     private static Item register(String id, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(DrugMod.MOD_ID, id), item);
